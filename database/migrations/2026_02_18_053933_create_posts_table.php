5ee976b2604fa10b6d1cfa937de179676d0fa83d
Thadshajini29 <sajiiselvi@gmail.com>
2026-02-20 11:31:54 +0530

    Initial commit

---
+++ database/migrations/2026_02_18_053933_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('category_id');
+            $table->string('color');
+            $table->string('image');
+            $table->string('body');
+            $table->string('tags');
+            $table->string('published')->default(false);
+            $table->string('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
